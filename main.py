54995468c8b2ed887c2274291d21d9ff94ef9b7e
Federico Mione <federico.mione@campus.tu-berlin.de>
2025-02-04 15:04:49 -0300

    Actor-Critic adaptation - files reorganization

--- main.py
+++
@@ -1,8 +0,0 @@
-# ***************************************************************** #
-#            THIS IS THE MAIN FILE FOR THE TRAINING STEP            #
-# ***************************************************************** #
-
-import gymnasium as gym
-
-
-env = gym.make('BioprocessEnv-v0')

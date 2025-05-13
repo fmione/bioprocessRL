372bfc08890ecb14ffb043dbff493f2953576409
DESKTOP-RI4NJTB\Martin <martin.fr.luna@gmail.com>
2025-05-13 14:37:11 -0300

    For plotting

--- Test_model_individual.py
+++ Test_model_individual.py
@@ -5,6 +5,9 @@ import os
 from stable_baselines3 import PPO
 
 import KiwiGym_createEnv_v4F
+
+import matplotlib.pyplot as plt
+
 # %%
 if __name__=="__main__":
 
@@ -14,6 +17,7 @@ if __name__=="__main__":
     model_name="ppo_agent_4F"
     model=PPO.load(os.path.join(load_dir,model_name),device="cpu") 
 
+    
     for i in range(1):
         env = gym.make('kiwiGym-v4F') 
         obs,_=env.reset()    
@@ -28,6 +32,21 @@ if __name__=="__main__":
                 env.render()
                 reward_acc.append(reward)
                 break
-
-
-
+# %%
+        mbr=0 #0-2
+        species=4 #0-4
+        
+        if species !=3:
+            tt=env.unwrapped.kiwiGym.DD_historic[mbr]['time_sample']
+        else:
+            tt=env.unwrapped.kiwiGym.DD_historic[mbr]['time_sensor']
+        
+        
+        XX=env.unwrapped.kiwiGym.XX['sample'][mbr][species]
+        
+        tp=env.unwrapped.kiwiGym.DD_historic[mbr]['time_pulse']
+        Fp=env.unwrapped.kiwiGym.DD_historic[mbr]['Feed_pulse']
+        
+        plt.plot(tt,XX,'.')
+        plt.show()
+        plt.plot(tp,Fp)
